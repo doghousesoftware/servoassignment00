525296603cb4842302ac476baf3b3b8753383ee2
bryan weinstein <bryanweinstein@outlook.com>
2021-04-17 11:47:57 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,48 @@
 input.onButtonPressed(Button.A, function () {
-    if (position >= 0) {
+    if (position < 180) {
+        position += 20
+        servos.P0.setAngle(position)
+        servos.P0.stop()
+    } else {
+        position = 180
+    }
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    if (position > 0) {
         position += -20
         servos.P0.setAngle(position)
-    } else {
+        servos.P0.stop()
+    }
+    if (position == 0) {
+        position = 0
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
         pins.digitalWritePin(DigitalPin.P1, 1)
         pins.digitalWritePin(DigitalPin.P2, 0)
     }
-    servos.P0.stop()
-})
-input.onButtonPressed(Button.B, function () {
-    if (position <= 180) {
-        position += 20
-        servos.P0.setAngle(position)
-    } else {
-        pins.digitalWritePin(DigitalPin.P2, 1)
-        pins.digitalWritePin(DigitalPin.P1, 0)
-    }
-    servos.P0.stop()
 })
 let position = 0
 position = 0
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
+pins.digitalWritePin(DigitalPin.P1, 1)
+pins.digitalWritePin(DigitalPin.P2, 0)
